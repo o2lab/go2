1ed7e102d15bb3ea2165d169101df5f24b4c7d4d
dopelsunce <sosccc1@gmail.com>
2020-01-15 22:26:25 -0600

    check simple cases for SO

---
+++ race_checker/go.mod
@@ -0,0 +1,9 @@
+module github.com/o2lab/race-checker
+
+go 1.13
+
+require (
+	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/tools v0.0.0-20200115165105-de0b1760071a
+)
--- race_checker/main.go
+++ race_checker/main.go
@@ -24,30 +24,30 @@ type chanOp struct {
 }
 
 type analysis struct {
-	prog          *ssa.Program
-	pkgs          []*ssa.Package
-	mains         []*ssa.Package
-	result        *pointer.Result
-	chanOps       []chanOp
-	ptaConfig     *pointer.Config
-	fn2SummaryMap map[*ssa.Function]*fnSummary
-	goID2insMap map[int]*ssa.Go
+	prog                *ssa.Program
+	pkgs                []*ssa.Package
+	mains               []*ssa.Package
+	result              *pointer.Result
+	chanOps             []chanOp
+	ptaConfig           *pointer.Config
+	fn2SummaryMap       map[*ssa.Function]*fnSummary
+	goID2insMap         map[int]*ssa.Go
 	bb2SyncBlockListMap map[*ssa.BasicBlock][]*SyncBlock
 }
 
 type fnSummary struct {
-	accesses []accessInfo
+	accesses       []accessInfo
 	fromGoroutines []int
 }
 
 type accessInfo struct {
-	write bool
-	atomic bool
-	location ssa.Value
+	write       bool
+	atomic      bool
+	location    ssa.Value
 	instruction *ssa.Instruction
-	parent *fnSummary
-	bb *ssa.BasicBlock
-	index int // index in BasicBlock
+	parent      *fnSummary
+	bb          *ssa.BasicBlock
+	index       int // index in BasicBlock
 	//lockset []*ssa.Instruction // list of protecting locks
 	//preChanOp []chanOp // list of chan ops before the access
 	//postChanOp []chanOp // list of chan ops after the access
@@ -59,46 +59,6 @@ type SyncBlock struct {
 	start, end int // start and end index in bb, excluding the instruction at index end
 }
 
-func newSyncBlock(bb *ssa.BasicBlock) *SyncBlock {
-	return &SyncBlock{
-		bb:    bb,
-		start: 0,
-		end:   -1,
-	}
-}
-
-func newSyncBlockFrom(bb *ssa.BasicBlock, block *SyncBlock, index int) *SyncBlock {
-	block.end = index
-	return &SyncBlock{
-		bb:    bb,
-		start: index,
-		end:   -1,
-	}
-}
-
-// chanOps returns a slice of all the channel operations in the instruction.
-func chanOps(instr ssa.Instruction) []chanOp {
-	var ops []chanOp
-	switch instr := instr.(type) {
-	case *ssa.UnOp:
-		if instr.Op == token.ARROW {
-			ops = append(ops, chanOp{instr.X, types.RecvOnly, instr.Pos()})
-		}
-	case *ssa.Send:
-		ops = append(ops, chanOp{instr.Chan, types.SendOnly, instr.Pos()})
-	case *ssa.Select:
-		for _, st := range instr.States {
-			ops = append(ops, chanOp{st.Chan, st.Dir, st.Pos})
-		}
-	case ssa.CallInstruction:
-		cc := instr.Common()
-		if b, ok := cc.Value.(*ssa.Builtin); ok && b.Name() == "close" {
-			ops = append(ops, chanOp{cc.Args[0], types.SendRecv, cc.Pos()})
-		}
-	}
-	return ops
-}
-
 var (
 	Analysis *analysis
 )
@@ -106,57 +66,106 @@ var (
 func (a *analysis) getLastSyncBlock(bb *ssa.BasicBlock) *SyncBlock {
 	syncBlocks, ok := a.bb2SyncBlockListMap[bb]
 	if ok && len(syncBlocks) > 0 {
-		return syncBlocks[len(syncBlocks) - 1]
+		return syncBlocks[len(syncBlocks)-1]
 	}
 	return nil
 }
 
-func (a *analysis) appendSyncBlock(bb *ssa.BasicBlock, sblock *SyncBlock) {
+func (a *analysis) generateSyncBlock(bb *ssa.BasicBlock, index int, isLast bool) {
+	if isLast {
+		return
+	}
+	sblock := a.getLastSyncBlock(bb)
+	if sblock == nil {
+		log.Fatal("No last SyncBlock")
+	}
+	sblock.end = index
+
+	newSBlock := &SyncBlock{
+		bb:    bb,
+		start: index,
+		end:   -1,
+	}
 	syncBlocks := a.bb2SyncBlockListMap[bb]
-	a.bb2SyncBlockListMap[bb] = append(syncBlocks, sblock)
+	a.bb2SyncBlockListMap[bb] = append(syncBlocks, newSBlock)
+}
+
+func IsSyncOp(instr *ssa.Instruction) bool {
+	switch ins := (*instr).(type) {
+	case *ssa.Send:
+		return true
+	case *ssa.UnOp:
+		return ins.Op == token.ARROW
+		// TODO: we consider send and recv only for now. Chan close and Lock/unlock need to be considered!
+	}
+	return false
+}
+
+func (a *analysis) getSyncOpsBeforeAndAfter(acc accessInfo) (bef []ssa.Instruction,
+	aft []ssa.Instruction) {
+	bb := acc.bb
+	for _, sb := range a.bb2SyncBlockListMap[bb] {
+		instr := bb.Instrs[sb.end]
+		if IsSyncOp(&instr) {
+			if sb.end < acc.index {
+				bef = append(bef, instr)
+			} else if sb.end > acc.index {
+				aft = append(aft, instr)
+			}
+		}
+	}
+	return
 }
 
 func (a *analysis) visitInstr(bb *ssa.BasicBlock, index int, instruction ssa.Instruction, isLast bool) {
 	if !instruction.Pos().IsValid() {
 		return // Skip NoPos. Can such instruction take part in a race?
 	}
-	switch instruction.(type) {
+	switch instr := instruction.(type) {
 	case *ssa.Alloc:
 	case *ssa.UnOp:
-		instr := instruction.(*ssa.UnOp)
 		// read op
 		if instr.Op == token.MUL {
 			a.addAccessInfo(&instruction, instr.X, false, false, instruction.Parent(), index, bb)
+			// chan recv
+		} else if instr.Op == token.ARROW {
+			a.generateSyncBlock(bb, index, isLast)
+			a.chanOps = append(a.chanOps, chanOp{instr.X, types.RecvOnly, instr.Pos()})
 		}
 	case *ssa.Store:
 		// write op
-		instr := instruction.(*ssa.Store)
 		a.addAccessInfo(&instruction, instr.Addr, true, false, instruction.Parent(), index, bb)
 	case *ssa.Go:
-		//instr := instruction.(*ssa.Go)
-		sblock := a.getLastSyncBlock(bb)
-		if sblock == nil {
-			log.Fatal("No last SyncBlock")
+		//a.generateSyncBlock(bb, index, isLast)
+	case *ssa.Send:
+		a.generateSyncBlock(bb, index, isLast)
+		a.chanOps = append(a.chanOps, chanOp{instr.Chan, types.SendOnly, instr.Pos()})
+	case *ssa.Select:
+		a.generateSyncBlock(bb, index, isLast)
+		for _, st := range instr.States {
+			a.chanOps = append(a.chanOps, chanOp{st.Chan, st.Dir, st.Pos})
 		}
-		if !isLast {
-			a.appendSyncBlock(bb, newSyncBlockFrom(bb, sblock, index))
-		} else {
-			sblock.end = index
+	case ssa.CallInstruction:
+		cc := instr.Common()
+		// chan close
+		if b, ok := cc.Value.(*ssa.Builtin); ok && b.Name() == "close" {
+			a.generateSyncBlock(bb, index, isLast)
+			a.chanOps = append(a.chanOps, chanOp{cc.Args[0], types.SendRecv, cc.Pos()})
 		}
-	// TODO: process locks, chan ops
+		// TODO: process locks
 	}
 }
 
 func (a *analysis) addAccessInfo(ins *ssa.Instruction, location ssa.Value, write bool, atomic bool, parent *ssa.Function, index int, bb *ssa.BasicBlock) accessInfo {
 	summary := a.fn2SummaryMap[parent]
 	info := accessInfo{
-		write: write,
-		atomic: atomic,
-		location: location,
+		write:       write,
+		atomic:      atomic,
+		location:    location,
 		instruction: ins,
-		parent: summary,
-		index: index,
-		bb: bb,
+		parent:      summary,
+		index:       index,
+		bb:          bb,
 	}
 	summary.accesses = append(summary.accesses, info)
 	a.ptaConfig.AddQuery(location)
@@ -182,18 +191,26 @@ func (a *analysis) visitInstrs() {
 		log.Debug(fn)
 		for bi, b := range fn.Blocks {
 			log.Debug("  --", b)
-			isLast := bi == len(fn.Blocks) - 1
 			for index, instr := range b.Instrs {
 				log.Debug("    --", instr)
+				isLast := bi == (len(b.Instrs) - 1)
 				if _, ok := a.bb2SyncBlockListMap[b]; !ok {
-					a.bb2SyncBlockListMap[b] = []*SyncBlock{newSyncBlock(b)}
+					a.bb2SyncBlockListMap[b] = []*SyncBlock{&SyncBlock{
+						bb:    b,
+						start: 0,
+						end:   -1,
+					}}
 				}
 				a.visitInstr(b, index, instr, isLast)
-				for _, op := range chanOps(instr) {
-					a.chanOps = append(a.chanOps, op)
+				for _, op := range a.chanOps {
 					a.ptaConfig.AddQuery(op.ch)
 				}
 			}
+			sb := a.getLastSyncBlock(b)
+			if sb == nil {
+				log.Fatal("syncblock list is empty")
+			}
+			sb.end = len(b.Instrs) - 1
 		}
 	}
 }
@@ -294,8 +311,31 @@ func (a *analysis) checkPO(minAcc *accessInfo, maxAcc *accessInfo) bool {
 	return false
 }
 
+// TODO: 1. Factor out the check using function "doChannelPeers". 2. Check if send is blocking
+func (a *analysis) hasBlockingSendRecvMatch(bef []ssa.Instruction, aft []ssa.Instruction) bool {
+	for _, bi1 := range bef {
+		// check ai2 -> bi1 by blocking send/recv
+		if instr1, ok := bi1.(*ssa.UnOp); ok && instr1.Op == token.ARROW {
+			for _, ai2 := range aft {
+				if instr2, ok := ai2.(*ssa.Send); ok && a.sameAddress(&instr1.X, &instr2.Chan) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+// check if acc1 is so-ordered to acc2 or acc2 is so-ordered to acc1
+// TODO: support mutex op
+func (a *analysis) checkSOTwoWay(acc1 accessInfo, acc2 accessInfo) bool {
+	bef1, aft1 := a.getSyncOpsBeforeAndAfter(acc1)
+	bef2, aft2 := a.getSyncOpsBeforeAndAfter(acc2)
+	return a.hasBlockingSendRecvMatch(bef1, aft2) || a.hasBlockingSendRecvMatch(bef2, aft1)
+}
+
 func (a *analysis) getConflictingAccesses(sum1 *fnSummary, sum2 *fnSummary) [][2]accessInfo {
-	var res[][2]accessInfo
+	var res [][2]accessInfo
 	if parallel, _, _ := canRunInParallel(sum1, sum2, nil, nil); !parallel {
 		return res
 	}
@@ -304,7 +344,8 @@ func (a *analysis) getConflictingAccesses(sum1 *fnSummary, sum2 *fnSummary) [][2
 			if (acc1.write || acc2.write) &&
 				(!acc1.atomic || !acc2.atomic) &&
 				a.sameAddress(&acc1.location, &acc2.location) {
-				if _, minIDAcc, maxIDAcc := canRunInParallel(sum1, sum2, &acc1, &acc2); !a.checkPO(minIDAcc, maxIDAcc) {
+				_, minIDAcc, maxIDAcc := canRunInParallel(sum1, sum2, &acc1, &acc2)
+				if !a.checkPO(minIDAcc, maxIDAcc) && !a.checkSOTwoWay(*minIDAcc, *maxIDAcc) {
 					res = append(res, [2]accessInfo{*minIDAcc, *maxIDAcc})
 				}
 			}
@@ -342,6 +383,7 @@ func canRunInParallel(summary1 *fnSummary, summary2 *fnSummary, info1 *accessInf
 func (a *analysis) getConflictingPairs() ([][2]*ssa.Function, [][2]accessInfo) {
 	cg := a.result.CallGraph
 	goID := 0
+	// traverse cg in pre-order to label each function with a list of goroutine IDs
 	err := GraphVisitEdgesPreorder(cg, func(edge *callgraph.Edge) error {
 		if isSynthetic(edge) {
 			return nil
@@ -387,8 +429,10 @@ func (a *analysis) getConflictingPairs() ([][2]*ssa.Function, [][2]accessInfo) {
 	for fn := range a.fn2SummaryMap {
 		functions = append(functions, fn)
 	}
+
+	// filter conflicting access pairs from every pair of accesses within every pair of functions
 	var fnPairs [][2]*ssa.Function
-	var accPairs[][2]accessInfo
+	var accPairs [][2]accessInfo
 	for i, fn1 := range functions {
 		for j := 0; j < i; j++ {
 			fn2 := functions[j]
@@ -399,12 +443,8 @@ func (a *analysis) getConflictingPairs() ([][2]*ssa.Function, [][2]accessInfo) {
 				accPairs = append(accPairs, conflictAccPairs...)
 			}
 		}
- 	}
+	}
 	return fnPairs, accPairs
-}
-
-func (a *analysis) doChannelPeers(ptsets map[ssa.Value]pointer.Pointer) {
-
 }
 
 func init() {
@@ -413,9 +453,9 @@ func init() {
 
 func doAnalysis(args []string) error {
 	cfg := &packages.Config{
-		Mode:       packages.LoadAllSyntax,
-		Dir:        "",
-		Tests:      false,
+		Mode:  packages.LoadAllSyntax,
+		Dir:   "",
+		Tests: false,
 	}
 
 	initial, err := packages.Load(cfg, args...)
@@ -446,17 +486,17 @@ func doAnalysis(args []string) error {
 	}
 
 	Analysis = &analysis{
-		prog:          prog,
-		pkgs:          pkgs,
-		mains:         mains,
-		ptaConfig:	   config,
-		fn2SummaryMap: make(map[*ssa.Function]*fnSummary),
-		goID2insMap: make(map[int]*ssa.Go),
-		bb2SyncBlockListMap:make(map[*ssa.BasicBlock][]*SyncBlock),
+		prog:                prog,
+		pkgs:                pkgs,
+		mains:               mains,
+		ptaConfig:           config,
+		fn2SummaryMap:       make(map[*ssa.Function]*fnSummary),
+		goID2insMap:         make(map[int]*ssa.Go),
+		bb2SyncBlockListMap: make(map[*ssa.BasicBlock][]*SyncBlock),
 	}
 
 	Analysis.visitInstrs()
-
+	Analysis.printSyncBlocks()
 	result, err := pointer.Analyze(config)
 	if err != nil {
 		return err // internal error in pointer analysis
@@ -468,6 +508,16 @@ func doAnalysis(args []string) error {
 	log.Debug(fnPairs)
 	Analysis.printAccPairs(accPairs)
 	return nil
+}
+
+// test only
+func (a *analysis) printSyncBlocks() {
+	for bb, sbs := range a.bb2SyncBlockListMap {
+		log.Debug(bb.Parent().Name(), " ", bb.Index)
+		for i, sb := range sbs {
+			log.Debugf("  %d: start=%d, end=%d, %s", i, sb.start, sb.end, bb.Instrs[sb.end])
+		}
+	}
 }
 
 func (a *analysis) printAccPairs(accPairs [][2]accessInfo) {
