935a3da4f00244cc289835bad6eaa4497e4ad61a
dopelsunce <sosccc1@gmail.com>
2020-02-03 13:54:37 -0600

    update example

--- race_checker/tests/race_example1.go
+++ race_checker/tests/race_example1.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 var (
 	i int
 	j int
@@ -9,14 +7,16 @@ var (
 
 func main() {
 	i = 1
-	t := 2
-	fmt.Println(t)
 	go func() {
 		i = 1
-		t1 := 3
-		fmt.Println(t1)
 	}()
-	i = 2
+	foo1(&i)
+}
+
+func foo1(i *int) {
+	go func() {
+		*i = 2
+	}()
 }
 
 //func A() {
