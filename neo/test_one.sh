eda9bd980cfcfbafb0c6fde1b175e9dda426e9dc
dopelsunce <sosccc1@gmail.com>
2020-05-24 17:07:38 -0500

    neo: update Dockerfile

--- neo/test_one.sh
+++ neo/test_one.sh
@@ -1,11 +1,15 @@
 testgo() {
-    go test -race -json -count 1 -timeout 10s ./...
+  COUNT=${COUNT:-1}
+  TIMEOUT=${TIMEOUT:-10s}
+  OUT=${OUT:-log}
+  echo running "go test -race -json -count $COUNT -timeout $TIMEOUT ./... >../${OUT}/$1.$$"
+  go test -race -json -count $COUNT -timeout $TIMEOUT ./... >../${OUT}/$1.$$
 }
 
 test_one() {
   p=$1
   echo testing $p. output: ${p}.$$
-  (cd $p; testgo >../log/${p}.$$)
+  (cd $p; testgo $p)
 }
 
 declare -a projects=(kubernetes caddy beego prometheus tidb gogs grpc-go syncthing)
@@ -32,4 +36,37 @@ show_cur_race() {
 
 test_race() {
     (cd $GOROOT/src/runtime/race; go test -v -race race_test.go)
+}
+
+release_dist() {
+  src=${1:-go-pred}
+  rm -rf go-dist
+  mkdir go-dist
+  echo copying src files in $src
+  rsync -a --exclude=".*" $GOROOT go-dist
+  (cd go-dist; mv $src go; tar czf dist.tar.gz go;
+  echo uploading dist.tar.gz
+  github-release upload \
+    --user dopelsunce \
+    --repo go \
+    --tag neo \
+    --name dist.tar.gz \
+    --replace \
+    --file dist.tar.gz)
+}
+
+update_neo() {
+  dst=${1:-neo-ufo}
+  (cd $dst/compiler-rt/lib/tsan/go; \
+   git pull; \
+   CC=clang-9 ./buildgo.sh; \
+   cp race_linux_amd64.syso $GOROOT/src/runtime/race/race_linux_amd64.syso.neo)
+}
+
+update_dist() {
+  url="https://github.com/dopelsunce/go/releases/download/neo/dist.tar.gz"
+	wget -O go.tgz "$url"
+	tar -C /usr/local -xzf go.tgz
+	rm go.tgz
+	go version
 }
