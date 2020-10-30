73b542bbd9aa2f18226780eb30d0abec3db0614a
BorenZang <borenzang@gmail.com>
2020-10-30 12:45:06 -0700

    test case when channel send after the select statement

---
+++ race_checker/tests/select_goafter.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	x := 0
+	go func() {
+		x /* RACE Write */= 1
+		ch1 <- 1
+	}()
+	select {
+	case a := <-ch1:
+		x = a
+	case a := <-ch2:
+		x = a + 1
+		fmt.Println(x)
+	}
+	go func() {
+		ch2 <- 2
+	}()
+}
