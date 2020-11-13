df4922473964479cf8d9404efacfc16a8e40567d
zyq823 <yiqingzhao@tamu.edu>
2020-11-13 13:48:45 -0600

    modified test case

--- race_checker/tests/lock_twice.go
+++ race_checker/tests/lock_twice.go
@@ -8,13 +8,8 @@ import (
 func main() {
 	mumu := sync.RWMutex{}
 	x := 0
-	mumu.Lock()
-	go func() {
-		x += 1
-		mumu.Unlock()
-	}()
 
-	mumu.Lock() // block until unlock?
+	mumu.Lock()
 	go func() {
 		mumu.Lock() // bug in code
 		x += 1
