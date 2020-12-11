092f4dbba1339ca351febf50fe52bbd9d8f5e1b1
LornaLou <57814384+LornaLou@users.noreply.github.com>
2020-12-11 15:28:33 -0600

    add multiple sends test case

---
+++ race_checker/tests/two_sends_one_receive.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	x := 0
+	go func() {
+		x = 2
+		ch2 <- 2
+	}()
+	go func() {
+		x = 4
+		ch2 <- 1
+	}()
+	select {
+	case a := <-ch1:
+		x = a
+	case a := <-ch2:
+		x = a + 1
+	default:
+		x = 20
+		fmt.Println(x)
+	}
+}
