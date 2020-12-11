6af2ba829cc70c4a0e9ca6b81d5b2fdfd84e2250
zyq823 <yiqingzhao@tamu.edu>
2020-12-10 19:28:20 -0600

    test case for indeterminate serialization of events

--- race_checker/tests/select_all_ready.go
+++ race_checker/tests/select_all_ready.go
@@ -1,23 +1,22 @@
 package main
 
-import "fmt"
-
 func main() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 	x := 0
+	y := 0
 	go func() {
-		x /* RACE Write */= 1
+		x /* RACE Write */ = 1
 		ch1 <- 1
+	}()
+	go func() {
+		y /* RACE Write */ = 2
+		ch2 <- 2
 	}()
 	select {
 	case a := <-ch1:
-		x = a
+		y /* RACE Write */ = a + 200
 	case a := <-ch2:
-		x = a + 1
-		fmt.Println(x)
+		x /* RACE Write */ = a + 100
 	}
-	go func() {
-		ch2 <- 2
-	}()
 }
