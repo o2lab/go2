e89e38d7471b53ccb676a7e15661656e54a80956
zyq823 <yiqingzhao@tamu.edu>
2021-01-11 13:13:25 -0600

    excerpt of runc code to help debug

---
+++ race_checker/tests/runc_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/urfave/cli"
+)
+
+
+var checkpointCommand = cli.Command{
+	Name:  "checkpoint",
+	Usage: "checkpoint a running container",
+	ArgsUsage: `<container-id>
+
+Where "<container-id>" is the name for the instance of the container to be
+checkpointed.`,
+	Description: `The checkpoint command saves the state of the container instance.`,
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "image-path", Value: "", Usage: "path for saving criu image files"},
+	},
+	Action: func(context *cli.Context) error {
+
+		//container, err := getContainer(context)
+		//if err != nil {
+		//	return err
+		//}
+		//
+		//return container.Checkpoint(options)
+		return nil
+	},
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "enable debug output for logging",
+		},
+		cli.StringFlag{
+			Name:  "log",
+			Value: "",
+			Usage: "set the log file path where internal debug information is written",
+		},
+	}
+	app.Commands = []cli.Command{
+		checkpointCommand,
+	}
+
+}
