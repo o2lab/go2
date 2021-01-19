ffa42a1c0086351dc65cb221e488c469c1246129
zyq823 <yiqingzhao@tamu.edu>
2021-01-19 12:47:36 -0600

    some refactoring

--- race_checker/analysisTools.go
+++ race_checker/analysisTools.go
@@ -2,10 +2,204 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+	"github.com/twmb/algoimpl/go/graph"
 	"strings"
 	"github.tamu.edu/April1989/go_tools/go/ssa"
 	"fmt"
 )
+
+func (a* analysis) buildHB(HBgraph *graph.Graph) {
+	var prevN graph.Node
+	var goCaller []graph.Node
+	var selectN []graph.Node
+	var readyCh []string
+	var selCaseEndN []graph.Node
+	var ifN []graph.Node
+	var ifSuccEndN []graph.Node
+	waitingN := make(map[*ssa.Call]graph.Node)
+	chanRecvs := make(map[string]graph.Node) // map channel name to graph node
+	chanSends := make(map[string]graph.Node) // map channel name to graph node
+	for nGo, insSlice := range a.RWIns {
+		for i, anIns := range insSlice {
+			disjoin := false // detach select case statement from subsequent instruction
+			insKey := goIns{ins: anIns, goID: nGo}
+			if nGo == 0 && i == 0 { // main goroutine, first instruction
+				prevN = HBgraph.MakeNode() // initiate for future nodes
+				*prevN.Value = insKey
+				if _, ok := anIns.(*ssa.Go); ok {
+					goCaller = append(goCaller, prevN) // sequentially store go calls in the same goroutine
+				}
+			} else {
+				currN := HBgraph.MakeNode()
+				*currN.Value = insKey
+				if nGo != 0 && i == 0 { // worker goroutine, first instruction
+					prevN = goCaller[0] // first node in subroutine
+					goCaller = goCaller[1:]
+				} else if _, ok := anIns.(*ssa.Go); ok {
+					goCaller = append(goCaller, currN) // sequentially store go calls in the same goroutine
+				} else if selIns, ok1 := anIns.(*ssa.Select); ok1 {
+					selectN = append(selectN, currN) // select node
+					readyCh = a.selReady[selIns]
+					selCaseEndN = []graph.Node{} // reset slice of nodes when encountering multiple select statements
+					readys := 0
+					for ith, ch := range readyCh {
+						if ch != "" && selIns.States[ith].Dir == 1 {
+							readys++
+							if _, ok0 := a.selUnknown[selIns]; ok0 && readys == 1 {
+								chanSends[ch] = currN
+							}
+						}
+					}
+				} else if ins, chR := anIns.(*ssa.UnOp); chR {
+					if ch := a.getRcvChan(ins); ch != "" { // a channel receive Op
+						chanRecvs[a.getRcvChan(ins)] = currN
+						if a.isReadySel(ch) { // channel waited on by select
+							disjoin = true // no edge between current node and node of succeeding instruction
+						}
+					}
+				} else if insS, chS := anIns.(*ssa.Send); chS {
+					chanSends[a.getSndChan(insS)] = currN
+				} else if _, isIf := anIns.(*ssa.If); isIf {
+					ifN = append([]graph.Node{currN}, ifN...) // store if statements
+				}
+				if ch, ok0 := a.selectCaseEnd[anIns]; ok0 && sliceContainsStr(readyCh, ch) {
+					selCaseEndN = append(selCaseEndN, currN)
+				}
+				if _, isSuccEnd := a.ifSuccEnd[anIns]; isSuccEnd {
+					ifSuccEndN = append(ifSuccEndN, currN)
+				}
+				// edge manipulation:
+				if ch, ok := a.selectCaseBegin[anIns]; ok {
+					if ch == "defaultCase" || ch == "timeOut" {
+						err := HBgraph.MakeEdge(selectN[0], currN) // select node to default case
+						if err != nil {
+							log.Fatal(err)
+						}
+					} else {
+						if _, ok1 := chanRecvs[ch]; ok1 {
+							err := HBgraph.MakeEdge(chanRecvs[ch], currN) // receive Op to ready case
+							if err != nil {
+								log.Fatal(err)
+							}
+						} else if sliceContainsStr(readyCh, ch) {
+							err := HBgraph.MakeEdge(selectN[0], currN) // select node to assumed ready cases
+							if err != nil {
+								log.Fatal(err)
+							}
+						}
+					}
+				} else if _, ok1 := a.selectDone[anIns]; ok1 {
+					if len(selCaseEndN) > 1 { // more than one portal is ready
+						err := HBgraph.MakeEdge(selectN[0], currN) // select statement to select done
+						if err != nil {
+							log.Fatal(err)
+						}
+					} else if len(selCaseEndN) > 0 {
+						err := HBgraph.MakeEdge(selCaseEndN[0], currN) // ready case to select done
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+					if selectN != nil && len(selectN) > 1 {
+						selectN = selectN[1:]
+					} // completed analysis of one select statement
+				} else if ifInstr, ok2 := a.ifSuccBegin[anIns]; ok2 {
+					skipSucc := false
+					for beginIns, ifIns := range a.ifSuccBegin {
+						if ifIns == ifInstr && beginIns != anIns && sliceContainsInsAt(a.commIfSucc, beginIns) != -1 && channelComm { // other succ contains channel communication
+							if (anIns.Block().Comment == "if.then" && beginIns.Block().Comment == "if.else") || (anIns.Block().Comment == "if.else" && beginIns.Block().Comment == "if.then") {
+								skipSucc = true
+								a.omitComm = append(a.omitComm, anIns.Block())
+							}
+						}
+					}
+					if !skipSucc {
+						err := HBgraph.MakeEdge(ifN[0], currN)
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+				} else {
+					err := HBgraph.MakeEdge(prevN, currN)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+				if !disjoin {
+					prevN = currN
+				}
+			}
+			// Create additional edges:
+			if a.isReadIns(anIns) || isWriteIns(anIns) {
+				a.RWinsMap[insKey] = prevN
+			} else if callIns, ok := anIns.(*ssa.Call); ok { // taking care of WG operations. TODO: identify different WG instances
+				if callIns.Call.Value.Name() == "Wait" {
+					waitingN[callIns] = prevN // store Wait node for later edge creation TO this node
+				} else if callIns.Call.Value.Name() == "Done" {
+					for wIns, wNode := range waitingN {
+						if a.sameAddress(callIns.Call.Args[0], wIns.Call.Args[0]) {
+							err := HBgraph.MakeEdge(prevN, wNode) // create edge from Done node to Wait node
+							if err != nil {
+								log.Fatal(err)
+							}
+						}
+					}
+				}
+			} else if dIns, ok1 := anIns.(*ssa.Defer); ok1 {
+				if dIns.Call.Value.Name() == "Done" {
+					for wIns, wNode := range waitingN {
+						if a.sameAddress(dIns.Call.Args[0], wIns.Call.Args[0]) {
+							err := HBgraph.MakeEdge(prevN, wNode) // create edge from Done node to Wait node
+							if err != nil {
+								log.Fatal(err)
+							}
+						}
+					}
+				}
+			}
+			if sendIns, ok := anIns.(*ssa.Send); ok && channelComm { // detect matching channel send operations
+				for ch, sIns := range a.chanSnds {
+					if rcvN, matching := chanRecvs[ch]; matching && sliceContainsSnd(sIns, sendIns) {
+						err := HBgraph.MakeEdge(prevN, rcvN) // create edge from Send node to Receive node
+						if err != nil {
+							log.Fatal(err)
+						}
+						err1 := HBgraph.MakeEdge(rcvN, prevN) // create edge from Send node to Receive node
+						if err1 != nil {
+							log.Fatal(err1)
+						}
+					}
+				}
+			} else if rcvIns, chR := anIns.(*ssa.UnOp); chR && channelComm {
+				if ch := a.getRcvChan(rcvIns); ch != "" {
+					if sndN, matching := chanSends[ch]; matching {
+						err := HBgraph.MakeEdge(sndN, prevN) // create edge from Send node to Receive node
+						if err != nil {
+							log.Fatal(err)
+						}
+						err1 := HBgraph.MakeEdge(prevN, sndN) // create edge from Send node to Receive node
+						if err1 != nil {
+							log.Fatal(err1)
+						}
+					}
+				}
+			}
+			if reIns, isReturn := anIns.(*ssa.Return); isReturn {
+				if a.ifFnReturn[reIns.Parent()] == reIns { // this is final return
+					for r, ifEndN := range ifSuccEndN {
+						if r != len(ifSuccEndN)-1 {
+							err := HBgraph.MakeEdge(ifEndN, prevN)
+							if err != nil {
+								log.Fatal(err)
+							}
+						}
+					}
+					ifSuccEndN = []graph.Node{} // reset slice containing last ins of each succ block preceeding final return
+				}
+			}
+		}
+	}
+}
 
 // visitAllInstructions visits each line and calls the corresponding helper function to drive the tool
 func (a *analysis) visitAllInstructions(fn *ssa.Function, goID int) {
