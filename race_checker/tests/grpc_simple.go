c187b76029c9242adc4b7e048803997ed618764d
zyq823 <yiqingzhao@tamu.edu>
2021-02-08 09:27:19 -0600

    fix testcase

--- race_checker/tests/grpc_simple.go
+++ race_checker/tests/grpc_simple.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"google.golang.org/grpc"
 )
 
-func someStream() string {
-	someInfo := "blah"
-	return someInfo + "er"
-}
-
 func someFunc(cs1 grpc.ClientStream) int {
-	err23 := cs1.SendMsg(someStream())
+	err23 := cs1.SendMsg(cs1)
 	if err23 != nil {
 		return 1
 	}
@@ -20,8 +16,14 @@ func someFunc(cs1 grpc.ClientStream) int {
 
 func main() {
 	var cs1 grpc.ClientConn
-	stream, err := cs1.NewStream()
-	_ = err
+	var ctx context.Context
+	var desc *grpc.StreamDesc
+	var method string
+	var opts grpc.CallOption
+	stream, err := cs1.NewStream(ctx, desc, method, opts)
+	if err != nil {
+		fmt.Println(err)
+	}
 	go someFunc(stream)
 	var err1 = stream.CloseSend()
 	if err1 != nil {
