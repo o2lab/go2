e30f107b6935729b87fbd750fe4d0aeecd8dc84c
zyq823 <yiqingzhao@tamu.edu>
2021-03-12 14:28:30 -0600

    WIP

--- race_checker/pointerAnalysis.go
+++ race_checker/pointerAnalysis.go
@@ -18,7 +18,7 @@ func (a *analysis) pointerAnalysis(location ssa.Value, goID int, theIns ssa.Inst
 			return
 		}
 	}
-	indir := false // toggle for indirect query (global variables)
+	//indir := false // toggle for indirect query (global variables)
 	if pointer.CanPoint(location.Type()) {
 		if useDefaultPTA {
 			a.mu.Lock()
@@ -26,13 +26,13 @@ func (a *analysis) pointerAnalysis(location ssa.Value, goID int, theIns ssa.Inst
 			a.mu.Unlock()
 		}
 	} else if underType, ok := location.Type().Underlying().(*types.Pointer); ok && pointer.CanPoint(underType.Elem()) {
-		indir = true
+		//indir = true
 		if useDefaultPTA {
 			a.ptaCfg0.AddIndirectQuery(location)
 		}
 	}
 
-	var ptrSet []pointer.PointerWCtx
+	//var ptrSet []pointer.PointerWCtx
 	var pta0Set map[ssa.Value]pta0.Pointer
 	var PT0Set []*pta0.Label
 	if useDefaultPTA {
@@ -87,40 +87,48 @@ func (a *analysis) pointerAnalysis(location ssa.Value, goID int, theIns ssa.Inst
 			break
 		}
 	} else { // new PTA
-		ptrSet = a.ptaRes[a.main].Queries[location] // set of pointers (with context) from ptaRes of pointer analysis
-		if indir {
-			ptrSetIndir := a.ptaRes[a.main].IndirectQueries[location]
-			_ = ptrSetIndir// TODO: check these labels
-		}
 
-		var fnName string
-		rightLoc := 0       // initialize index for the right points-to location
-		rightCtx := 0 // initialize index for the pointer with right context
-		if len(ptrSet) > 1 { // multiple targets returned by pointer analysis
-			//log.Trace("***Pointer Analysis revealed ", len(PTSet), " targets for location - ", a.prog.Fset.Position(location.Pos()))
-			var fns []string
-			for ind, eachPtr := range ptrSet { // check each pointer (with context)
-				//log.Debug(eachPtr.GetMyContext())
-				ptsLabels := eachPtr.PointsTo().Labels() // set of labels for locations that the pointer (with context) points to
-				for _, eachLabel := range ptsLabels {
-					if eachLabel.Value().Parent() != nil {
-						fns = append(fns, eachLabel.Value().Parent().Name())
-						//log.Trace("*****target No.", ind+1, " - ", eachPtr.Value().Name(), " from function ", eachPtr.Value().Parent().Name())
-						if sliceContainsStr(a.storeIns, eachLabel.Value().Parent().Name()) { // calling function is in current goroutine
-							rightLoc = ind
-							break
-						}
-					} else {
-						continue
-					}
-				}
-			}
-			//log.Trace("***Executing target No.", rightLoc+1)
-		} else if len(ptrSet) == 0 {
-			//log.Debug("Points-to set is empty: " + location.String() + " @ " + theIns.String())
-			return
+		//ptrSet = a.ptaRes[a.main].Queries[location] // set of pointers (with context) from ptaRes of pointer analysis
+		//if indir {
+		//	ptrSetIndir := a.ptaRes[a.main].IndirectQueries[location]
+		//	_ = ptrSetIndir// TODO: check these labels
+		//}
+		//
+		//var fnName string
+		//rightLoc := 0       // initialize index for the right points-to location
+		//rightCtx := 0 // initialize index for the pointer with right context
+		//if len(ptrSet) > 1 { // multiple targets returned by pointer analysis
+		//	//log.Trace("***Pointer Analysis revealed ", len(PTSet), " targets for location - ", a.prog.Fset.Position(location.Pos()))
+		//	var fns []string
+		//	for ind, eachPtr := range ptrSet { // check each pointer (with context)
+		//		//log.Debug(eachPtr.GetMyContext())
+		//		ptsLabels := eachPtr.PointsTo().Labels() // set of labels for locations that the pointer (with context) points to
+		//		for _, eachLabel := range ptsLabels {
+		//			if eachLabel.Value().Parent() != nil {
+		//				fns = append(fns, eachLabel.Value().Parent().Name())
+		//				//log.Trace("*****target No.", ind+1, " - ", eachPtr.Value().Name(), " from function ", eachPtr.Value().Parent().Name())
+		//				if sliceContainsStr(a.storeIns, eachLabel.Value().Parent().Name()) { // calling function is in current goroutine
+		//					rightLoc = ind
+		//					break
+		//				}
+		//			} else {
+		//				continue
+		//			}
+		//		}
+		//	}
+		//	//log.Trace("***Executing target No.", rightLoc+1)
+		//} else if len(ptrSet) == 0 {
+		//	//log.Debug("Points-to set is empty: " + location.String() + " @ " + theIns.String())
+		//	return
+		//}
+		//labels := ptrSet[rightLoc].PointsTo().Labels()
+		var ptr pointer.PointerWCtx
+		if goID == 0 {
+			ptr = a.ptaRes[a.main].PointsToByGo(location, nil)
+		} else {
+			ptr = a.ptaRes[a.main].PointsToByGo(location, a.RWIns[goID][0].(*ssa.Go))
 		}
-		labels := ptrSet[rightLoc].PointsTo().Labels()
+		labels := ptr.PointsTo().Labels()
 		if labels == nil {
 			//bz: if nil, probably from reflection, which we excluded from analysis;
 			// meanwhile, most benchmarks do not use reflection, this is probably infeasible path
@@ -129,7 +137,8 @@ func (a *analysis) pointerAnalysis(location ssa.Value, goID int, theIns ssa.Inst
 			return
 		}
 
-		switch theFunc := labels[rightCtx].Value().(type) {
+		var fnName string
+		switch theFunc := labels[0].Value().(type) {
 		case *ssa.Function:
 			fnName = theFunc.Name()
 			if !a.exploredFunction(theFunc, goID, theIns) {
@@ -139,10 +148,10 @@ func (a *analysis) pointerAnalysis(location ssa.Value, goID int, theIns ssa.Inst
 			}
 		case *ssa.MakeInterface:
 			methodName := theIns.(*ssa.Call).Call.Method.Name()
-			if a.prog.MethodSets.MethodSet(ptrSet[rightLoc].PointsTo().DynamicTypes().Keys()[0]).Lookup(a.mains[0].Pkg, methodName) == nil { // ignore abstract methods
+			if a.prog.MethodSets.MethodSet(ptr.PointsTo().DynamicTypes().Keys()[0]).Lookup(a.mains[0].Pkg, methodName) == nil { // ignore abstract methods
 				break
 			}
-			check := a.prog.LookupMethod(ptrSet[rightLoc].PointsTo().DynamicTypes().Keys()[0], a.mains[0].Pkg, methodName)
+			check := a.prog.LookupMethod(ptr.PointsTo().DynamicTypes().Keys()[0], a.mains[0].Pkg, methodName)
 			fnName = check.Name()
 			if !a.exploredFunction(check, goID, theIns) {
 				a.updateRecords(fnName, goID, "PUSH ")
