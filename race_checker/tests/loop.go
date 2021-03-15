bdce6fbde65f4e6b25b06f42275d0192c7a96868
zyq823 <yiqingzhao@tamu.edu>
2021-03-15 08:42:45 -0500

    test case for context sensitivity

--- race_checker/tests/loop.go
+++ race_checker/tests/loop.go
@@ -1,1 +1,14 @@
 package main
+
+type some_struct struct {
+	some_field int
+}
+func main() {
+	for i := 0; i < 2; i++ {
+		go func() {
+			a := &some_struct{some_field: 0}
+			a.some_field++ // no race here but go instruction are identical (wrong context)
+		}()
+	}
+}
+
