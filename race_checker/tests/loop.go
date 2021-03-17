c334d3a77086d47c43bdbd85b235148342e92cb3
zyq823 <yiqingzhao@tamu.edu>
2021-03-17 17:40:18 -0500

    add test cases

--- race_checker/tests/loop.go
+++ race_checker/tests/loop.go
@@ -4,8 +4,8 @@ type some_struct struct {
 	some_field int
 }
 func main() {
-	for i := 0; i < 2; i++ {
-		go func() {
+	for i2 := 0; i2 < 2; i2++ {
+		go func() { // context
 			a := &some_struct{some_field: 0}
 			a.some_field++ // no race here but go instruction are identical (wrong context)
 		}()
