a14355ab6380fcfdbd67aae8ac9491beb73a6d11
zyq823 <yiqingzhao@tamu.edu>
2021-05-20 23:15:37 -0500

    revise race printout

--- gorace/printSource.go
+++ gorace/printSource.go
@@ -36,17 +36,30 @@ func printVarName(rwPos token.Position) string {
 	lineRmTabs := strings.TrimLeftFunc(theLine, unicode.IsSpace)
 	tabs := len(theLine)-len(lineRmTabs)
 	spaces := rwPos.Column-tabs // should be minimum 0
-	var isStringAlphabetic = regexp.MustCompile(`^[a-zA-Z0-9_]`).MatchString
+	var isStringAlphanumeric = regexp.MustCompile(`^[a-zA-Z0-9_]`).MatchString
 	for i := spaces; i < len(lineRmTabs); i++ {
-		if !isStringAlphabetic(lineRmTabs[i:i+1]) {
-			if spaces < 1 {
-				spaces = 1
+		if !isStringAlphanumeric(lineRmTabs[i:i+1]) { // character is not alphanumeric
+			if lineRmTabs[i:i+1] == "]" { // array/map element
+				for j := spaces - 1; j >= 0; j-- { // backtrack to find beginning
+					if j == 0 {
+						return lineRmTabs[:i+1]
+					}
+					if !isStringAlphanumeric(lineRmTabs[j-1:j]) {
+						return lineRmTabs[j:i+1]
+					}
+				}
 			}
-			return lineRmTabs[spaces-1:i] // TODO: test result to make sure it looks correct
+			if spaces < 1 {
+				return lineRmTabs[:i]
+			}
+			return lineRmTabs[spaces-1:i]
+		}
+		if i == len(lineRmTabs)-1 { // reaching end of line
+			return lineRmTabs[spaces-1:]
 		}
 	}
-	if spaces < 1 {
-		spaces = 1
+	if spaces < 1 { // shouldn't be reaching here really
+		return lineRmTabs[:spaces]
 	}
 	return lineRmTabs[spaces-1:spaces]
 }
@@ -57,9 +70,18 @@ func printSource(rwPos token.Position) {
 		if lineNum == rwPos.Line {
 			tabs := len(theLine)-len(strings.TrimLeftFunc(theLine, unicode.IsSpace))
 			spaces := rwPos.Column-tabs
-			log.Info("\t>> ", strings.Repeat("\t", tabs), strings.Repeat(" ", spaces-1), "v")
+			if spaces > 0 && strings.TrimLeftFunc(theLine, unicode.IsSpace)[spaces-1:spaces] == "[" {
+				log.Info("\t>> ", strings.Repeat("\t", tabs), strings.Repeat(" ", spaces), "v")
+			} else {
+				log.Info("\t>> ", strings.Repeat("\t", tabs), strings.Repeat(" ", spaces-1), "v")
+			}
 			log.Info("\t>> ", theLine)
-			log.Info("\t>> ", strings.Repeat("\t", tabs), strings.Repeat(" ", spaces-1), "^")
+			if spaces > 0 && strings.TrimLeftFunc(theLine, unicode.IsSpace)[spaces-1:spaces] == "[" {
+				log.Info("\t>> ", strings.Repeat("\t", tabs), strings.Repeat(" ", spaces), "^")
+			} else {
+				log.Info("\t>> ", strings.Repeat("\t", tabs), strings.Repeat(" ", spaces-1), "^")
+			}
+
 		} else {
 			log.Info("\t>> ", theLine)
 		}
